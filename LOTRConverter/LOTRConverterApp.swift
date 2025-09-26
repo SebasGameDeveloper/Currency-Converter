fe0856c920a1205a854f2b47bdf65bb3764d4add
Sebastian Martinez <jsebastianmartinezmarin@gmail.com>
2025-09-26 09:30:25 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Juan Martinez on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
